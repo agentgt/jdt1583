30b5123d04a2cee7612be1ea6674de19c0e56369
Adam Gent <adam.gent@snaphop.com>
2023-11-21 16:34:42 -0500

    JDT issue 1583

---
+++ src/jdt1583/Stuff.java
@@ -0,0 +1,12 @@
+package jdt1583;
+
+/**
+ * 
+ * {@snippet :
+ * public class Blah {
+ * }
+ * }
+ */
+public class Stuff {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * asdfasdfs
+ * asdfasdf
+ * 
+ * {@snippet stuff class="snippets.Blah" region="provider" :
+ * public class Blah {
+ * }
+ * }
+ */
+module jdt1583 {
+}
